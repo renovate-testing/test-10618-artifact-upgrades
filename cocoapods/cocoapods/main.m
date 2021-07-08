6ac46e6d0bb8b6eafe18b61282e94540408378aa
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-07-08 12:44:19 +0300

    cocoapods

---
+++ cocoapods/cocoapods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocoapods
+//
+//  Created by Sergio Zharinov on 08.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
